55cc911ea68ba2801b4be2474a54470e2abccbf9
BmLatin0 <73851025+BMA1656@users.noreply.github.com>
2022-10-21 13:18:13 -0600

    Update main.js

--- main.js
+++ main.js
@@ -13,4 +13,33 @@ function mayorN(){
     }  
     
     console.log(mayorN())
-
+    
+    
+    function palindromo(palab){ 
+      let array =[]; 
+      let array2=[];
+       for(let i of palab) {
+       array.push(i);
+    }   for (let i = array.length - 1; i> -1; i--){
+            array2.push(array[i]);  
+    }
+        
+     
+    for (let i = 0; i < array.length; i++) {
+      for (let j = 0; j < array[i].length; j++) {
+         for (let i = 0; i < array2.length; i++) {
+          for (let j = 0; j < array2[i].length; j++) {
+          if (array[i][j] != array2[i][j]){
+            return 'No es un palindromo'
+          }
+            else {
+              return 'Es un palindromo'
+            }
+      }
+    }
+      }
+    }
+    
+    }  
+    console.log(palindromo('casa'));
+    
