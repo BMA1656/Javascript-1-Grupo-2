4755e73411cebd62550129b909c4333392c64111
BmLatin0 <73851025+BMA1656@users.noreply.github.com>
2022-11-28 18:30:52 -0600

    a

---
+++ quizzDOM/main.js
@@ -0,0 +1,107 @@
+// let data = [
+//   {
+//     id: 7,
+//     email: "michael.lawson@reqres.in",
+//     age: 30,
+//     userAccount: "Premium",
+//     first_name: "Michael",
+//     last_name: "Lawson",
+//     description: "Lorem ipsum",
+//   },
+//   {
+//     id: 8,
+//     email: "lindsay.ferguson@reqres.in",
+//     age: 78,
+//     userAccount: "Basic",
+//     first_name: "Lindsay",
+//     last_name: "Ferguson",
+//     description: "Lorem ipsum",
+//   },
+//   {
+//     id: 9,
+//     email: "tobias.funke@reqres.in",
+//     age: 23,
+//     userAccount: "Basic",
+//     first_name: "Tobias",
+//     last_name: "Funke",
+//     description: "Lorem ipsum",
+//   },
+//   {
+//     id: 10,
+//     email: "byron.fields@reqres.in",
+//     age: 86,
+//     userAccount: "Basic",
+//     first_name: "Byron",
+//     last_name: "Fields",
+//     description: "Lorem ipsum",
+//   },
+//   {
+//     id: 11,
+//     email: "george.edwards@reqres.in",
+//     age: 30,
+//     userAccount: "Premium",
+//     first_name: "George",
+//     last_name: "Edwards",
+//     description: "Lorem ipsum",
+//   },
+//   {
+//     id: 12,
+//     email: "rachel.howell@reqres.in",
+//     age: 67,
+//     userAccount: "Premium",
+//     first_name: "Rachel",
+//     last_name: "Howell",
+//     description: "Lorem ipsum",
+//   },
+//   {
+//     id: 13,
+//     email: "adrian.bell@reqres.in",
+//     age: 54,
+//     userAccount: "Basic",
+//     first_name: "Adrian",
+//     last_name: "Bell",
+//     description: "Lorem ipsum",
+//   },
+//   {
+//     id: 14,
+//     email: "adrian.bell@reqres.in",
+//     age: 54,
+//     userAccount: "",
+//     userAccount: "Adrian",
+//     last_name: "Bell",
+//     description: "Lorem ipsum",
+//   },
+// ];
+// function imprimirElementos() {
+//   var carta = document.getElementById("mainContainer");
+//   var id = document.createElement("h2");
+//   var nombre = document.createElement("h3");
+//   var user = document.createElement("h4");
+//   var email = document.createElement("p");
+//   var descripcion = document.createElement("p");
+
+//   data.forEach(imprimirElementos => {
+   
+//     data.id + data.email + data.lastname + data.description + data.first_name = data
+//   });
+
+//   id.appendChild(data.id);
+// }
+// imprimirElementos();
+
+
+<!-- <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<div class="mainContainer">
+
+</div>
+</body>
+<script src="main.js"></script>
+</html> --></meta>
